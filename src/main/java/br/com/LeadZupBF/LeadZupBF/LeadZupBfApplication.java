e5225ee33c5249fc84e2790af4a1b9155f8f4ffb
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-11-18 10:49:51 -0300

    Adicionar configuração de banco de dados

---
+++ src/main/java/br/com/LeadZupBF/LeadZupBF/LeadZupBfApplication.java
@@ -0,0 +1,13 @@
+package br.com.LeadZupBF.LeadZupBF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadZupBfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadZupBfApplication.class, args);
+	}
+
+}
